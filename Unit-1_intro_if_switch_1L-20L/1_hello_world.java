21b1a6080e191fd7b544190fa6363b72146ea92e
mihirgupta665 <mihirgupta665@gmail.com>
2025-09-29 11:02:25 +0530

    hello world

---
+++ Unit-1_intro_if_switch_1L-20L/1_hello_world.java
@@ -0,0 +1,13 @@
+//Staring the Java
+//running file and byte code without code runner.
+class intro{
+    public static void main(String ars[]){
+        System.out.println("Hello World! Welcome to my amazing world of JAVA");
+    }
+}
+/*
+copiling java code using terminal : 
+javac file_name.java
+java class_name
+(usually file name is givem same as class name)
+*/
