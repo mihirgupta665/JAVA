1c2f203c921140997f34ef26438383bd5844212c
mihirgupta665 <mihirgupta665@gmail.com>
2025-10-02 14:33:16 +0530

    package

---
+++ Unit-3_Inheritance_Abstract_Interface_49L-74L/C2_packages.java
@@ -0,0 +1,5 @@
+//package is like a folder which is used to collect adn store related classes.[A Package could contain a package within itself.]
+//For Ex: import package_name.class_name  [to import in the currect file; if you want to create a package insteat of importing then just remove the word "import"]
+//java.lang.* [x: imports all teh files of that package] is by default is already imported in evry java file so there is no need to mention it.
+//default(no access specifer mentioned) then that could be accessed in the same package
+// Go through the picture to understand adn learn access specifier.
