50af4e3167986295276cabb7a5795dd53bc538e3
mihirgupta665 <mihirgupta665@gmail.com>
2025-10-10 20:27:48 +0530

    Map Filter Sorted

---
+++ Unit-6/F6_Map_Filter_Sorted.java
@@ -0,0 +1,7 @@
+//WAP to implemet map, filter and Sorted using Arrays
+//parallelStream instead of Stream are used to make many thread to perform same action
+public class F6_Map_Filter_Sorted {
+    public static void main(String args[]){
+
+    }
+}
