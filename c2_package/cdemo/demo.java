a829844640655733b76d82ee4c726e821b58ed42
mihirgupta665 <mihirgupta665@gmail.com>
2025-10-12 23:47:54 +0530

    c2_package

---
+++ c2_package/cdemo/demo.java
@@ -0,0 +1,11 @@
+package cdemo;
+import ccalc.calc.*;
+class demo{
+    public static void main(String args[]){
+        Advclac obj= new Advclac();
+        obj.add();
+        obj.diff();
+        obj.multi();
+    }
+}
+
